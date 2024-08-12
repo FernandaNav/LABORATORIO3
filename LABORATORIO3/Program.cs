0ad5147397445513e3e7a1fdb867773815412b15
Fernanda Navarro <fernnavarrom22@gmail.com>
2024-08-12 08:57:38 -0600

    Inicio de la estructura de switch

--- LABORATORIO3/Program.cs
+++ LABORATORIO3/Program.cs
@@ -2,14 +2,51 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("-----------------");
-        Console.WriteLine("   BIENVENIDO");
-        Console.WriteLine("-----------------");
-        Console.WriteLine("1. Registrar cliente");
-        Console.WriteLine("2. Registrar vehículo");
-        Console.WriteLine("3. Registrar Pedido");
-        Console.WriteLine("4. Mostrar información");
-        Console.WriteLine("5. Buscar información");
-        Console.WriteLine("6. Salir");
+        int opcion = 0;
+        do
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("       BIENVENIDO");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("1. Registrar cliente");
+            Console.WriteLine("2. Registrar vehículo");
+            Console.WriteLine("3. Registrar Pedido");
+            Console.WriteLine("4. Mostrar información");
+            Console.WriteLine("5. Buscar información");
+            Console.WriteLine("6. Salir");
+            Console.WriteLine();
+            Console.Write("Ingresa una opción: ");
+            try
+            {
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                //mensaje de error
+            }
+        } while (opcion != 6);
     }
 }
