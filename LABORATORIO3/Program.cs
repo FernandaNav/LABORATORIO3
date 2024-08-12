14328cdedd6c05bf33c3caa055d367494d948106
Fernanda Navarro <Fernanda Navarro@FERNANDA>
2024-08-12 08:46:18 -0600

    Agregar archivos de proyecto.

---
+++ LABORATORIO3/Program.cs
@@ -0,0 +1,15 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-----------------");
+        Console.WriteLine("   BIENVENIDO");
+        Console.WriteLine("-----------------");
+        Console.WriteLine("1. Registrar cliente");
+        Console.WriteLine("2. Registrar vehículo");
+        Console.WriteLine("3. Registrar Pedido");
+        Console.WriteLine("4. Mostrar información");
+        Console.WriteLine("5. Buscar información");
+        Console.WriteLine("6. Salir");
+    }
+}
