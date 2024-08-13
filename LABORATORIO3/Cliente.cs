70806759b08ae1115a73a3fff7d33660687d1b6a
Fernanda Navarro <fernnavarrom22@gmail.com>
2024-08-12 18:12:05 -0600

    Creé otro proyecto para empezar la opción b del laboratorio

---
+++ LABORATORIO3/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO3
+{
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+
+        public Cliente(string nombre, string correo, string direccion)
+        {
+            Nombre = nombre;
+            Correo = correo;
+            Direccion = direccion;
+        }
+        public virtual void MostrarInformacion()
+        {
+
+        }
+    }
+}
